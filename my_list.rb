b02bd212b5a0d1a49cfe412074c24db17da945ae
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-06-04 01:58:37 +0300

    Fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -7,9 +7,9 @@ class MyList
     @list = elements
   end
 
-  def each
+  def each(&block)
     return enum_for(__method__) unless block_given?
 
-    @list.each { |element| yield element }
+    @list.each(&block)
   end
 end
